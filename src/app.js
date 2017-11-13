939248834414b4ad8a5141c4e295fbe5f535646c
Mohsin <mohammadmohsin@gmail.com>
2017-11-13 13:48:35 +0500

    Setting up NotFound page route.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import TableNumber from './components/TableNumber';
 
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404!</h1>
+    </div>
+  )
+}
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path='/' component={TableNumber} exact={true} />
       <Route path='/dashboard' component={Dashboard} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 )
 
