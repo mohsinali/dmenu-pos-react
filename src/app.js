c5a1d5c4008a942d3fa1b1264abf83b9f17962d8
Mohsin <mohammadmohsin@gmail.com>
2017-11-14 13:01:13 +0500

    Inspinia theme implementation

--- src/app.js
+++ src/app.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 
-
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/components/TableNumber.js
+++ src/components/TableNumber.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class TableNumber extends React.Component {
-  render(){
+  render() {
     return (
-      <div>
-        <form>
-          <input type="text" name='txtTableNumber' placeholder='Table Number' />
-        </form>
+      <div className="row  border-bottom white-bg dashboard-header">
+        <div className="col-sm-1">
+
+        </div>
+        <div className="col-sm-6">
+
+          <div className="jumbotron">
+            <form>
+              <h1>
+                <input type="text" name='txtTableNumber' placeholder='Table Number' />
+              </h1>
+            </form>
+          </div>
+
+        </div>
+        <div className="col-sm-1">  </div>
       </div>
+
     )
   }
 }
