03e3bdd822fbb0498ab5edca346360224d1e99ae
Mohsin <mohammadmohsin@gmail.com>
2017-11-14 14:14:49 +0500

    Separating Left nav and header

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import LeftNav from './components/LeftNav';
+import MainHeader from './components/MainHeader';
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(<LeftNav />, document.getElementById('left-nav'));
+ReactDOM.render(<MainHeader />, document.getElementById('main-header'));
